c20bd75cc39b4824c05fa829aa9c72c9a2b46972
LIM JING HAO RP <itsbenjaminsam@gmail.com>
2020-05-11 18:22:07 +0800

    tesy

---
+++ FYP/wwwroot/Models/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP.wwwroot.Models
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Please enter User ID")]
+        public string UserID { get; set; }
+
+        [Required(ErrorMessage = "Please enter Password")]
+        public string Password { get; set; }
+    }
+}
