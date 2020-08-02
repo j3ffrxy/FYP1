d7082eb3af82cc546cea6841627a31002979ac5e
shotgamer07@gmail.com <shotgamer07@gmail.com>
2020-08-02 19:19:04 +0800

    finished eq maintenance

---
+++ FYP/Models/Maintenance.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYP.Models
+{
+    public class Maintenance
+    {
+        public int Maintenance_id { get; set; }
+
+        public string Serial_no { get; set; }
+
+        [Required(ErrorMessage = "Enter a valid date")]
+        [DataType(DataType.DateTime)]
+        public DateTime Start_date { get; set; }
+
+        [Required(ErrorMessage = "Enter a valid date")]
+        [DataType(DataType.DateTime)]
+        public DateTime End_date { get; set; }
+
+        public string maint_type { get; set; }
+
+        public string description { get; set; }
+
+        public bool archive { get; set; }
+    }
+}
